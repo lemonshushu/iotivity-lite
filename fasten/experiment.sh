ca0630bd7ab7b23863aca735b17ff4a3238a1750
lemonshushu <lemonshushu@lemonshushu.me>
2023-10-08 15:25:28 +0900

    add experiment.sh

---
+++ fasten/experiment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script runs an experiment with the fasten_client program using different resource types.
+# It loops through a list of resource types with different suffixes and runs the fasten_client program 10 times for each resource type.
+
+rt_prefix=fasten_
+rt_suffix_list=("128" "256" "512" "1K" "2K" "4K")
+rt_list=()
+
+for suffix in ${rt_suffix_list[@]}; do
+    temp=$rt_prefix$suffix
+    rt_list+=($temp)
+done
+
+for rt in ${rt_list[@]};
+do
+    for i in {1..10};
+    do
+        ./fasten_client $rt
+    done
+done
